d72259af93f098d21809702b9ffc43b048ffa2f9
hfmckenna <hfmckenna@outlook.com>
2022-08-16 14:43:40 +0100

    Add structs to treehouse

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,5 +1,22 @@
 use std::io::stdin;
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+    fn greet_visitor(&self) {
+        println!("{} {}, welcome!", self.greeting, self.name);
+    }
+}
+
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
@@ -11,18 +28,22 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
-    let visitor_list = ["jim", "davey", "gabbo"];
+    let visitor_list = [
+        Visitor::new("John", "Hello"),
+        Visitor::new("Mary", "Good morning"),
+        Visitor::new("Joe", "Good evening"),
+    ];
+
     println!("Hello, what's your name?");
     let name = what_is_your_name();
     let mut allow_them_in = false;
     for visitor in &visitor_list {
-        if visitor == &name {
+        if visitor.name == name {
             allow_them_in = true;
+            visitor.greet_visitor();
         }
     }
-    if allow_them_in {
-        println!("Welcome to the Treehouse, {}", name);
-    } else {
+    if !allow_them_in {
         println!("Sorry, you aren't on the list.");
     }
 }
