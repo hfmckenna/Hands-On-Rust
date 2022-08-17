44d2d8a2cd108e4681f3419238f15742d824d411
Hugh McKenna <hfmckenna@outlook.com>
2022-08-17 11:50:38 +0100

    Add player to state

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -1,5 +1,9 @@
 use bracket_lib::prelude::*;
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
 enum GameMode {
     Menu,
     Playing,
@@ -7,12 +11,22 @@ enum GameMode {
 }
 
 struct State {
+    player: Player,
     mode: GameMode,
+    frame_time: f32,
+}
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
 }
 
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
@@ -21,21 +35,70 @@ impl State {
         self.mode = GameMode::End;
     }
     fn restart(&mut self) {
-        // TODO: Fill this stub out later
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
-    fn main_menu(&mut self) {
-        // TODO: Fill this stub out later
-        self.mode = GameMode::Menu;
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Yer deid!");
+        ctx.print_centered(8, "(P) Play game");
+        ctx.print_centered(9, "(Q) Quit game");
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play game");
+        ctx.print_centered(9, "(Q) Quit game");
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x: 0,
+            y: 0,
+            velocity: 0.0,
+        }
+    }
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'))
+    }
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0
+        }
+    }
+    fn flap(&mut self) {
+        self.velocity = -2.0;
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         match self.mode {
-            GameMode::Menu => self.main_menu(),
+            GameMode::Menu => self.main_menu(ctx),
             GameMode::Playing => self.play(ctx),
-            GameMode::End => self.restart(),
+            GameMode::End => self.dead(ctx),
         }
     }
 }
